fb60f13e8c3e9918a9191ac09e6478fc97a9820e
danyparc <danyparc@gmail.com>
2020-05-29 09:31:49 -0500

    improve test name

--- util.test.js
+++ util.test.js
@@ -1,6 +1,7 @@
 const { generateText } = require('./util');
-
-test('should output name and age', () => {
+describe('generateText function', () => {
+  test('When passing valid values should, it should output name and age in a friendly way', () => {
     const text = generateText('Max', 29);
     expect(text).toBe('Max (29 years old)');
-});
+  });
+})
