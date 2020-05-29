8b5f196f76f592c32eb66b56a9ac0eba631b5d78
danyparc <danyparc@gmail.com>
2020-05-29 09:45:44 -0500

    improve test description

--- util.test.js
+++ util.test.js
@@ -1,29 +1,40 @@
 const puppeteer = require('puppeteer');
 const { generateText, checkAndGenerate } = require('./util');
 
-test('should output name and age', () => {
-  const text = generateText('Max', 29);
-  expect(text).toBe('Max (29 years old)');
-});
-
-test('should generate a valid text output', () => {
-  const text = checkAndGenerate('Max', 29);
-  expect(text).toBe('Max (29 years old)');
-});
-
-test('should create an element with text and correct class', async () => {
-  const browser = await puppeteer.launch({
-    headless: true,
-    // slowMo: 80,
-    // args: ['--window-size=1920,1080']
+describe('generateText function', () => {
+  test('When passing valid values should, it should output name and age in a friendly way', () => {
+    const text = generateText('Max', 29);
+    expect(text).toBe('Max (29 years old)');
   });
-  const page = await browser.newPage();
-  await page.goto('file://' + __dirname + '/index.html');
-  await page.click('input#name');
-  await page.type('input#name', 'Anna');
-  await page.click('input#age');
-  await page.type('input#age', '28');
-  await page.click('#btnAddUser');
-  const finalText = await page.$eval('.user-item', el => el.textContent);
-  expect(finalText).toBe('Anna (28 years old)');
-}, 10000);
+})
+
+describe('checkAndGenerate function', () => {
+  test('When passing valid values, should generate a valid text output', () => {
+    const text = checkAndGenerate('Max', 29);
+    expect(text).toBe('Max (29 years old)')
+  });
+  test('When passing invalid values, it should return false', () => {
+    const text = checkAndGenerate('', 0);
+    expect(text).toBe(false)
+  });
+});
+
+describe('add user', ()=>{
+  test('When inputs has valid values and add button is clicked, it should create an element with text and correct class', async () => {
+    const browser = await puppeteer.launch({
+      headless: false,
+      // slowMo: 80,
+      // args: ['--window-size=1920,1080']
+    });
+    const page = await browser.newPage();
+    await page.goto('file://' + __dirname + '/index.html');
+    await page.click('input#name');
+    await page.type('input#name', 'Anna');
+    await page.click('input#age');
+    await page.type('input#age', '28');
+    await page.click('#btnAddUser');
+    const finalText = await page.$eval('.user-item', el => el.textContent);
+    expect(finalText).toBe('Anna (28 years old)');
+  }, 10000);
+
+})
