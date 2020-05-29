b54b13ac1b907fcff0e53634fa69cf39d1f2bae7
danyparc <danyparc@gmail.com>
2020-05-29 09:39:53 -0500

    add descriptions and one more test

--- util.test.js
+++ util.test.js
@@ -1,11 +1,19 @@
 const { generateText, checkAndGenerate } = require('./util');
 
-test('should output name and age', () => {
-  const text = generateText('Max', 29);
-  expect(text).toBe('Max (29 years old)');
-});
+describe('generateText function', () => {
+  test('When passing valid values should, it should output name and age in a friendly way', () => {
+    const text = generateText('Max', 29);
+    expect(text).toBe('Max (29 years old)');
+  });
+})
 
-test('should generate a valid text output', () => {
-  const text = checkAndGenerate('Max', 29);
-  expect(text).toBe('Max (29 years old)')
+describe('checkAndGenerate function', () => {
+  test('When passing valid values, should generate a valid text output', () => {
+    const text = checkAndGenerate('Max', 29);
+    expect(text).toBe('Max (29 years old)')
+  });
+  test('When passing invalid values, it should return false', () => {
+    const text = checkAndGenerate('', 0);
+    expect(text).toBe(false)
+  });
 });
